b0968fcb34d06487c9344f9f62db2c6f0b4f361d
louyue.xyf <louyue.xyf@alibaba-inc.com>
2017-12-01 17:34:27 +0800

    正在建设中

--- front/src/router.js
+++ front/src/router.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const TestList = () => import('./views/TestList.vue')
-const TestView = () => import('./views/TestView.vue')
+// const TestList = () => import('./views/TestList.vue')
+// const TestView = () => import('./views/TestView.vue')
 const Home = () => import('@/views/Home.vue')
+const Waiting = () => import('@/views/Waiting.vue')
 
 export function createRouter () {
   return new Router({
@@ -13,8 +14,12 @@ export function createRouter () {
     fallback: false,
     scrollBehavior: () => ({y: 0}),
     routes: [
-      {path: '/archive', component: TestList},
-      {path: '/test/:id', component: TestView},
+      {path: '/article', component: Waiting},
+      {path: '/category', component: Waiting},
+      {path: '/archive', component: Waiting},
+      {path: '/bbs', component: Waiting},
+      {path: '/about', component: Waiting},
+      // {path: '/test/:id', component: TestView},
       {path: '/', component: Home}
     ]
   })
---
+++ front/src/views/Waiting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test-list">
+    建设中...
+  </div>
+</template>
+
+<script>
+export default {
+  title () {
+    return '建设中...'
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  
+</style>
