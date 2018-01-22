a19f3e49c684307f7a59e6150a8ace945c6971ed
louyue.xyf <louyue.xyf@alibaba-inc.com>
2018-01-22 16:28:09 +0800

    定调

--- admin/.postcssrc.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-module.exports = {
-  plugins: {
-    'autoprefixer': {
-      browsers: [ 'last 3 versions', '> 1%']
-    }
-  }
-}
--- admin/src/store/types.js
+++
@@ -1,6 +0,0 @@
-import createModule from '@util/createModule'
-
-export const test = createModule('test', {
-  A: ['FETCH_ITEMS'],
-  M: ['UPDATE_ITEMS']
-})
